e147f97308b582f5f800e0892fb9cba139ae6f35
Wichai W. <wichaikwon@gmail.com>
2025-02-19 13:37:31 +0700

    add .env file

--- main.go
+++ main.go
@@ -4,11 +4,17 @@ package main
 import (
 	"golang-api/config"
 	"golang-api/controllers"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
 	config.ConnectToDB()
 	r := gin.Default()
 	r.GET("/users", controllers.GetUsers)
